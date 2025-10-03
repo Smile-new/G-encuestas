5a827bde4ad34d42a9b28c4cda092b65123ce05c
Hadescito <sthefan-salvatore@outlook.es>
2025-10-02 19:19:19 -0600

    Update estadisticas.php

--- app/Views/admin/estadisticas.php
+++ app/Views/admin/estadisticas.php
@@ -195,7 +195,7 @@
                     </a>
                 </li>
                 <li class="nav-item menu-items">
-                    <a class="nav-link" href="<?= base_url('estadisticas') ?>">
+                    <a class="nav-link" href="<?= base_url('estadistica') ?>">
                         <span class="menu-icon"><i class="mdi mdi-chart-bar"></i></span>
                         <span class="menu-title">Estadísticas</span>
                     </a>
